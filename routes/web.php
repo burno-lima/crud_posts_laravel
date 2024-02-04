f5918a6e2f65d46269933e0c740de3739a8786dc
Bruno Lima <brunoluislima19@gmail.com>
2024-02-04 00:01:22 -0300

    feat: create page e route posts

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\URL;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +13,18 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+URL::forceScheme('https');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', PostController::class .'@index')->name('posts.index');
+
+Route::get('/posts/create', PostController::class . '@create')->name('posts.create');
+
+Route::post('/posts', PostController::class . '@store')->name('posts.store');
+
+Route::get('/posts/{post}', PostController::class . '@show')->name('posts.show');
+
+Route::get('/posts/{posts}/edit', PostController::class . '@edit')->name('posts.edit');
+
+Route::put('/posts/{post}', PostController::class . '@update')->name('posts.update');
+
+Route::delete('/posts/{post}', PostController::class . '@destroy')->name('posts.destroy');
